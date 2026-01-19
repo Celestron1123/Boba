54797bd16fe26f9e3471fc7d93b709a671770f9b
Elijah Potter <epot1123@gmail.com>
2026-01-19 15:47:51 -0700

    Initial Commit

---
+++ Boba/BobaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BobaApp.swift
+//  Boba
+//
+//  Created by Elijah Potter on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct BobaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
